89bf29b3f83e90c05e1f44a2f857ef26df403833
offensive-vk <data.offensive.gov@gmail.com>
2023-10-16 22:03:34 +0530

    Content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import logo from '../assets/logo.jpg';
 
 function Footer() {
     return (
         <footer>
             <div className="company">
-                <img src="radioactive.jpg" alt="undefined" /><br />
+                <img src={logo} alt="undefined" /><br />
                 <div className="brand">Radioactive, Inc</div>
                 <h3>Code. Test. Deploy.</h3>
                 <h4>&copy; {new Date().getFullYear()} Copyright by (Authors). All Rights Reserved.</h4>
