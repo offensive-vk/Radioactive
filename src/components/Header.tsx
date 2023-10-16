c20558d5773c1eeb5755c100c5be1cef25e7e21a
offensive-vk <data.offensive.gov@gmail.com>
2023-10-16 22:02:23 +0530

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import logo from '../assets/logo.jpg';
 
 function Header() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <div className="logo">{logo}</div>
+            <nav>
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#author">Author</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#codes">Codes</a></li>
+                </ul>
+            </nav>
+            {/* <div className="theme-switcher">
+                <input type="checkbox" id="theme-toggle" />
+                <label htmlFor="theme-toggle"></label>
+            </div> */}
+        </header>
+    );
 }
 
 export default Header;
