3b689bb091c3230e6a5d45e085e9f82bd9b0d2fd
offensive-vk <data.offensive.gov@gmail.com>
2023-10-18 18:51:09 +0530

    Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.jpg';
+
+function Loader() {
+  return (
+    <div id="loader">
+        <img src={logo} alt="undefined" />
+    </div>
+  )
+}
+
+export default Loader
