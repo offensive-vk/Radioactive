f3ea1eed733178b72d3df63e948eb04178614ac4
offensive-vk <data.offensive.gov@gmail.com>
2023-10-20 17:25:03 +0530

    Loaders

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import logo from '../assets/logo.jpg';
 import '../styles/Loader.scss';
 
 function Loader() {
- 
+  useEffect(() => {
+    window.addEventListener('DOMContentLoaded', () => {
+      const loading: HTMLElement | null = document.getElementById('loader');
+      setTimeout(() => {
+        if (loading) {
+          loading.style.display = 'none';
+        }
+      }, 4500);
+    });
+  }, []); // An empty dependency array means this effect runs once when the component mounts
+
   return (
     <div id="loader">
+      <div id="progress-bar"></div>
       <img src={logo} alt="Logo" />
     </div>
   );
