f70cab3f4db52e2ab25ec14e8ab675796dce8309
offensive-vk <data.offensive.gov@gmail.com>
2023-10-20 15:38:45 +0530

    Fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,33 @@ function Footer() {
                 <img src={logo} alt="undefined" /><br />
                 <div className="brand">Radioactive, Inc</div>
                 <h3>Code. Test. Deploy.</h3>
+                <h3>Nuclear Tech, Atomic Innovation</h3>
                 <h4>&copy; {new Date().getFullYear()} Copyright by (Authors). All Rights Reserved.</h4>
             </div>
-            {/* <div className="socials">
+            <div className="socials">
                 <ul>
-                    <li><a href="https://github.com" target="_blank" rel="noopener noreferrer"></a></li>
-                    <li><a href="https://gitlab.com" target="_blank" rel="noopener noreferrer"></a></li>
-                    <li><a href="https://vercel.com" target="_blank" rel="noopener noreferrer"></a></li>
-                    <li><a href="https://radioactive.com" target="_blank" rel="noopener noreferrer"></a></li>
+                    <li>
+                        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://gitlab.com" target="_blank" rel="noopener noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://npmjs.org" target="_blank" rel="noopener noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://debian.org" target="_blank" rel="noopener noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/debian/debian-original.svg" />
+                        </a>
+                    </li>
                 </ul>
-            </div> */}
+            </div>
             <hr />
             <h4>Crafted & Created with ❤️ by Author.</h4>
         </footer>
