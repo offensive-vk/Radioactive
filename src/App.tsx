6eb47efae00ffd3493d52eed52bdcabe1e33e0aa
offensive-vk <data.offensive.gov@gmail.com>
2023-10-26 18:24:27 +0530

    ~

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './styles/App.scss';
 import './styles/Global.scss';
-import logo from './assets/logo.jpg';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import SectionHome from './components/SectionHome';
 
-// import Loader from './components/Loader';
 
 function App() {
-  console.log(`Found Logo @ ${logo}\n`);
+  console.log(`Welcome To Console @ ${undefined}.\n`);
   return (
     <>
       <Header />
