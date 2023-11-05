a39abe63f9ee3505193c8cca61e1407033a1932d
offensive-vk <data.offensive.gov@gmail.com>
2023-11-05 22:37:27 +0530

    Few Changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,32 @@
 import '../styles/Home.scss';
 
-function Code<Type>(text: Type, isClosing: boolean = false): Type{
-  return !isClosing? (`&lt; ${text} &gt;`) as Type: (`&lt; ${text} /&gt;`) as Type;
+function Code(props: string, isClosing: boolean = false){
+  if (isClosing) return <div className='closing'>&lt; {props} /&gt;</div>
+  else return <div className='opening'>&lt; {props} &gt;</div>
 }
 
 function Home() {
   return (
     <>
+
     <div className='text subheading'>
-      Nuclear Tech, Atomic Innovation
+      Nuclear Tech, Atomic Innovation 
     </div>
     <div className='explain'>
       <div className='box _code'>
-        &lt; Code &gt;<br />Crafting digital poetry with logic and language of your choice.
+        {Code('Code')}<br />
+          Crafting digital poetry with logic and language of your choice. Best in class is Typescript.
+        {Code('Code', true)}
       </div>
       <div className='box _test'>
-        &lt; Test &gt; <br/>The important detective work that ensures your creation is flawless.
+        {Code('Test')}<br/>
+          The important detective work that ensures your creation is flawless. Works perfectly fine.
+        {Code('Test', true)}
       </div>
       <div className='box _deploy'>
-        &lt; Deploy &gt; <br/>
-        Releasing your digital masterpiece into the world, like setting a ship to sail on a new adventure.
+        {Code('Deploy')} <br/>
+          Releasing your digital masterpiece into the world, like a ship to sail on a new adventure. 
+        {Code('Deploy', true)}
       </div>
     </div>
     </>
