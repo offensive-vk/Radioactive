f5ffd50e9f2eda066ca28335aa079b5c264f14a6
offensive-vk <data.offensive.gov@gmail.com>
2023-11-17 19:49:51 +0530

    Minors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,15 +3,14 @@ import { useNavigate } from "react-router-dom";
 function NotFound() {
   const navigate = useNavigate();
   return (
-    <div className="w-full flex flex-col items-center justify-center my-[25vh]">
+    <div className="w-full flex flex-col items-center justify-center">
       <h1 className="font-bold text-red-600 text-5xl">404</h1>
       <p>{"I'm sorry but is that URL right?"}</p>
       <button
         onClick={() => {
           navigate("/");
         }}
-        className="bg-black my-2 px-5 rounded-md hover:bg-gray-800 transition-colors text-white"
-      >
+        className="">
         Go Home
       </button>
     </div>
