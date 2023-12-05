f76ed0edd603e418188ccf5d661daf1f110956d8
offensive-vk <data.offensive.gov@gmail.com>
2023-12-05 18:19:14 +0530

    Minor Fixes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 function NotFound() {
   const navigate = useNavigate();
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="font-bold text-red-600 text-5xl">404</h1>
+    <div className="404-wrapper w-full flex flex-col items-center justify-center">
+      <h1 className="404-heading font-bold text-red-600 text-5xl">404</h1>
       <p>{"I'm sorry but is that URL right?"}</p>
       <button
         onClick={() => {
           navigate("/");
         }}
-        className="">
+        className="404-button">
         Go Home
       </button>
     </div>
